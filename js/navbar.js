47879070e1af14eec532974fd539e6bf049a11ce
Edgar de Dios <eduardoddg60@gmail.com>
2019-07-08 02:21:48 -0500

    Navbar mobile agregada

--- js/navbar.js
+++ js/navbar.js
@@ -11,20 +11,19 @@ for (i; i < link.length; i++) {
 }
 
 
-
 window.onscroll = function () {
     if (document.documentElement.scrollTop > 150) {
         header.className = 'header-up';
         bcHeader.className = 'nav-shadow-down';
-        for(var i=0;i<link.length;i++){
-            link[i].style.color = "#76F0BA";
+        for (var i = 0; i < link.length; i++) {
+            link[i].style.color = "#53DCF0";
         }
         stateScroll = 1;
     }
     else if (stateScroll == 1) {
         header.className = 'header-down';
         bcHeader.className = 'nav-shadow-up';
-        for(var i=0;i<link.length;i++){
+        for (var i = 0; i < link.length; i++) {
             link[i].style.color = "white";
         }
         stateScroll = 0;
@@ -33,7 +32,7 @@ window.onscroll = function () {
 
 function changeColor() {
     if (stateScroll == 0) {
-        this.style.color = "rgb(34, 34, 34)";
+        this.style.color = "#53DCF0";
     } else {
         this.style.color = "white";
     }
@@ -43,8 +42,8 @@ function changeColor() {
 function resetColor() {
     if (stateScroll == 0) {
         this.style.color = "white";
-    }else{
-        this.style.color = "#76F0BA";
+    } else {
+        this.style.color = "#53DCF0";
     }
 
 }
@@ -53,100 +52,14 @@ function resetColor() {
 
 
 // movile device button
-// var buttonMenu = document.getElementById("btnMenuSection");
-// var itemsSection = document.getElementById("itemsSection");
+var buttonMenu = document.getElementById("menuSection");
+var buttonCloseMenu = document.getElementById("buttonCloseMenu");
+var menuMobile = document.getElementById("menuMobile");
 
 
-// var state = 'disable';
-
-// function resetMenu() {
-//     if (screen.width > 750) {
-//         itemsSection.style.display = 'flex';
-//         state = 'enable';
-//     } else if (screen.width <= 750) {
-//         itemsSection.style.display = 'none';
-//         state = 'disable';
-//     }
-// }
-
-// function showMenu() {
-//     if (state == 'enable') {
-//         itemsSection.style.display = 'flex';
-//     } else if (state == 'disable') {
-//         itemsSection.style.display = 'none';
-//     }
-// }
-
-// buttonMenu.addEventListener('click', changeState)
-
-// function changeState() {
-//     if (state == 'disable') {
-//         state = 'enable';
-//     } else if (state == 'enable') {
-//         state = 'disable';
-//     }
-//     showMenu();
-// }
-
-// function resetMenuOutItems() {
-//     var posXItem = itemsSection.getBoundingClientRect().x;
-//     var posYItem = itemsSection.getBoundingClientRect().y;
-//     var widthItem = itemsSection.offsetWidth;
-//     var heightItem = itemsSection.offsetHeight;
-//     var posXButton = buttonMenu.getBoundingClientRect().x;
-//     var posYButton = buttonMenu.getBoundingClientRect().y;
-//     var widthButton = buttonMenu.offsetWidth;
-//     var heightButton = buttonMenu.offsetHeight;
-//     var x = window.event.clientX;
-//     var y = window.event.clientY;
-
-//     if (state == 'enable' && screen.width < 750) {
-
-//         if ((x >= posXButton && x <= posXButton + widthButton && y >= posYButton && y <= posYButton + heightButton) ||
-//             x >= posXItem && x <= posXItem + widthItem && y >= posYItem && y <= posYItem + heightItem) {
-//             // console.log('ok');
-//         } else {
-//             state = 'disable';
-//             itemsSection.style.display = 'none';
-//             // console.log('none');
-//         }
-//     }
-// }
-
-
-// Scroll Navbar
-// var header = document.getElementById("header");
-// var bcHeader = document.getElementById("bcHeader");
-// var nav = document.getElementById("nav");
-// var stateScroll = 0;
-
-// window.onscroll = function () {
-//     scroolls();
-// }
-
-
-// function scroolls() {
-//     var scrollState = 0;
-//     if (document.documentElement.scrollTop > 350) {
-//         header.className = "headerDown";
-//         header.style.top = '15px';
-//         stateScroll = 1;
-
-
-//         if (screen.width > 1530) {
-//             nav.className = "navDown";
-//         }
-//         bcHeader.className = "shadowDown";
-
-//     } else if (document.documentElement.scrollTop < 350 && stateScroll == 1) {
-//         header.className = "headerUp";
-//         header.style.top = '50px';
-//         stateScroll = 0;
-//         bcHeader.className = "shadowUp";
-//         if (screen.width > 1530) {
-//             nav.className = "navUp";
-//         }
-
-//     }
-//     // scrollCards(scrollState);
-// }
+buttonMenu.addEventListener('click', function(){
+    menuMobile.className = "container-menu-mobile-show";
+});
+buttonCloseMenu.addEventListener('click', function(){
+    menuMobile.className = "container-menu-mobile-hidden";
+});
