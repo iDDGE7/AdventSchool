9b6c2f1df4b478ab1ccd965cf13bea2f55d20e58
Edgar de Dios <eduardoddg60@gmail.com>
2019-07-10 20:34:09 -0500

    test slides

--- js/cardWellcome.js
+++ js/cardWellcome.js
@@ -12,11 +12,47 @@ var inicio = 0;
 var countcard = 1;
 var cardAnt = card1;
 var circleAnt = circle1;
+var timeOutSlider;
+var timerOut;
 
-circle1.addEventListener('click', changeCard);
-circle2.addEventListener('click', changeCard);
-circle3.addEventListener('click', changeCard);
-circle4.addEventListener('click', changeCard);
+var sliderCard = setInterval(changeCardAuto, 3000);
+
+circle1.addEventListener('click', function () {
+    changeCardAuto();
+    clearInterval(sliderCard);
+    clearTimeout(timerOut);
+    timerOut = setTimeout(function () {
+        sliderCard = setInterval(changeCardAuto, 3000);
+    }, 10000);
+    return this;
+});
+circle2.addEventListener('click', function () {
+    changeCardAuto();
+    clearInterval(sliderCard);
+    clearTimeout(timerOut);
+    timerOut = setTimeout(function () {
+        sliderCard = setInterval(changeCardAuto, 3000);
+    }, 10000);
+    return this;
+});
+circle3.addEventListener('click', function () {
+    changeCardAuto();
+    clearInterval(sliderCard);
+    clearTimeout(timerOut);
+    timerOut = setTimeout(function () {
+        sliderCard = setInterval(changeCardAuto, 3000);
+    }, 10000);
+    return this;
+});
+circle4.addEventListener('click', function () {
+    changeCardAuto();
+    clearInterval(sliderCard);
+    clearTimeout(timerOut);
+    timerOut = setTimeout(function () {
+        sliderCard = setInterval(changeCardAuto, 3000);
+    }, 10000);
+    return this;
+});
 
 
 for (var i = 0; i < cards.length; i++) {
@@ -24,7 +60,7 @@ for (var i = 0; i < cards.length; i++) {
     circles[i].style.transition = "all 0.5s";
 }
 
-function changeCard() {
+function changeCardButtons() {
     if (this == circle1 && countcard != 1) {
         countcard = 1;
         card1.style.transform = "translateX(0%)";
@@ -35,7 +71,6 @@ function changeCard() {
         circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
         circleAnt = circle1;
         cardAnt = card1;
-
     }
     else if (this == circle2 && countcard != 2) {
         countcard = 2;
@@ -70,6 +105,52 @@ function changeCard() {
         circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
         circleAnt = circle4;
         cardAnt = card4;
+
+    }
+}
+
+function changeCardAuto() {
+    if (countcard == 1) {
+        countcard = 2;
+        card1.style.transform = "translateX(0%)";
+        card2.style.transform = "translateX(100%)";
+        card3.style.transform = "translateX(200%)";
+        card4.style.transform = "translateX(300%)";
+
+        circle1.style.background = "rgb(22, 151, 184)";
+        circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
+        circleAnt = circle1;
+    }
+    else if (countcard == 2) {
+        countcard = 3;
+        card2.style.transform = "translateX(0%)";
+        card1.style.transform = "translateX(-100%)";
+        card3.style.transform = "translateX(100%)";
+        card4.style.transform = "translateX(200%)";
+        circle2.style.background = "rgb(22, 151, 184)";
+        circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
+        circleAnt = circle2;
+
+    }
+    else if (countcard == 3) {
+        countcard = 4;
+        card3.style.transform = "translateX(0%)";
+        card1.style.transform = "translateX(-200%)";
+        card2.style.transform = "translateX(-100%)";
+        card4.style.transform = "translateX(100%)";
+        circle3.style.background = "rgb(22, 151, 184)";
+        circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
+        circleAnt = circle3;
+    }
+    else if (countcard == 4) {
+        countcard = 1;
+        card4.style.transform = "translateX(0%)";
+        card1.style.transform = "translateX(-300%)";
+        card2.style.transform = "translateX(-200%)";
+        card3.style.transform = "translateX(-100%)";
+        circle4.style.background = "rgb(22, 151, 184)";
+        circleAnt.style.background = "rgba(255, 255, 255, 0.404)";
+        circleAnt = circle4;
 
     }
 }
